c8f795ab395195130b447500e42fc19bffb250e4
kolesenT <aniskova007@mail.ru>
2022-12-09 08:53:38 +0300

    HW_30_del

--- wp-content/themes/bootstrap/404.php
+++
@@ -1,6 +0,0 @@
-<?php get_header();?>
-
-<h1 class="my-4 page-title">Ошибка 404</h1>
-<p>Страница не найдена</p>
-
-<?php get_footer();?>
--- wp-content/themes/bootstrap/archive.php
+++
@@ -1,4 +0,0 @@
-<?php get_header(); ?>
-<h1> <?php the_tags(); ?> </h1>
-<?php get_footer(); ?>
-
